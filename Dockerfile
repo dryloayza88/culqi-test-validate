1bfebde360c79bc3260a52496696bd7efd51d5ac
Diego Yarleque <diego.yarleque@globant.com>
2020-05-17 21:32:20 -0500

    fixing docker push image name.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM openjdk:8
+RUN addgroup -S spring && adduser -S spring -G spring
+USER spring:spring
+ENTRYPOINT ["java", "-cp", "app:app/lib/x", "hello.TestApplication"]
 COPY /target/test-?.?*.jar .
 CMD java -jar test-?.?*.jar
--- src/main/java/com/culqi/test/TestApplication.java
+++ src/main/java/com/culqi/test/TestApplication.java
@@ -2,9 +2,15 @@ package com.culqi.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
+
+    public String hello() {
+        return "Hello Dockerized RESTful!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
